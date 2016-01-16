df25079530f415fc7bcea6efb8b89c703fbe8dec
danielferreira <daniel.dos.santos.ferreira@gmail.com>
2016-01-16 13:01:07 +0100

    Add Exercises helper

---
+++ Numbers-Teach-Android/app/src/main/java/com/dferreira/numbers_teach/helpers/ExercisesHelper.java
@@ -0,0 +1,155 @@
+package com.dferreira.numbers_teach.helpers;
+
+import java.util.Random;
+
+/**
+ * Helper to create the logic behind the exercise
+ */
+public class ExercisesHelper {
+
+    private static Random rnd = new Random();
+    private static int ARRAY_INIT_VALUE = -1;
+
+    /**
+     * @param iArray array o integers
+     * @param value  value to check if exists
+     * @return false : The array does not contain the value passed
+     * true : The array contains the value that was passed
+     */
+    private static boolean arrayContains(int[] iArray, int value) {
+        for (int i = 0; i < iArray.length; i++) {
+            if (iArray[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Generates an array of not repeated indexes
+     *
+     * @param min         Min value of the index
+     * @param max         Max value of the index
+     * @param totalSlides total of slides
+     * @return Array of indexes
+     */
+    public static int[] generateIndexes(int min, int max, int totalSlides) {
+        int maxNumber = max - min;
+        int[] genIndexes = new int[totalSlides];
+
+        //Avoid be trap by initialization of zeros
+        for (int i = 0; i < genIndexes.length; i++) {
+            genIndexes[i] = ARRAY_INIT_VALUE;
+        }
+
+        //Generate no repeated values
+        for (int i = 0; i < genIndexes.length; i++) {
+            int newValue;
+            do {
+                newValue = rnd.nextInt(maxNumber);
+            } while (arrayContains(genIndexes, newValue));
+            genIndexes[i] = newValue;
+        }
+
+        //Add the min value to set the values in the range
+        for (int i = 0; i < genIndexes.length; i++) {
+            genIndexes[i] = min + genIndexes[i];
+        }
+
+        return genIndexes;
+    }
+
+    /**
+     * Generate an array of possible options to the user select the right one
+     *
+     * @param rightIndex      right index to the user to select
+     * @param totalSlides     total of slides
+     * @param numberOfOptions Number of options show to the user
+     * @return Array of index of the selectable options
+     */
+    public static int[] generateIndexesOptions(int rightIndex, int totalSlides, int numberOfOptions) {
+
+        int[] genIndexes = new int[numberOfOptions];
+
+        //Avoid be trap by initialization of zeros
+        for (int i = 0; i < genIndexes.length; i++) {
+            genIndexes[i] = ARRAY_INIT_VALUE;
+        }
+
+        //Put the right value somewhere
+        genIndexes[rnd.nextInt(numberOfOptions)] = rightIndex;
+
+        //Generate no repeated values
+        for (int i = 0; i < genIndexes.length; i++) {
+            if(genIndexes[i] == ARRAY_INIT_VALUE) {
+                int newValue;
+                do {
+                    newValue = rnd.nextInt(totalSlides);
+                } while (arrayContains(genIndexes, newValue));
+                genIndexes[i] = newValue;
+            }
+        }
+
+
+        return genIndexes;
+
+    }
+
+    /**
+     * @param indexes Index of the images to show
+     * @return The paths of the 2D images to be show to the user taking in account the passed arguments
+     */
+    public static String[] getImages2DPath(int[] indexes) {
+        if ((indexes == null) || (indexes.length <= 0)) {
+            return null;
+        } else {
+            String[] paths = new String[indexes.length];
+            for (int i = 0; i < indexes.length; i++) {
+                paths[i] = FilePathHelper.getImg2DPath(indexes[i]);
+            }
+            return paths;
+        }
+    }
+
+    /**
+     * @param indexes Index of the images to show
+     * @return The paths of the 3D object to be show to the user taking in account the passed arguments
+     */
+    public static String[] getObjects3DPath(int[] indexes) {
+        if ((indexes == null) || (indexes.length <= 0)) {
+            return null;
+        } else {
+            String[] paths = new String[indexes.length];
+            for (int i = 0; i < indexes.length; i++) {
+                paths[i] = FilePathHelper.getObj3DPath(indexes[i]);
+            }
+            return paths;
+        }
+    }
+
+    /**
+     * @param index Index of the option
+     * @return The tag corresponding to the index passed
+     */
+    public static Object getTag(int index) {
+        return Integer.toString(index);
+    }
+
+    /**
+     * @param indexes Array of indexes to generate the tags
+     * @return Tags of the images to allow in the palette of images
+     */
+    public static Object[] getTags(int[] indexes) {
+        if ((indexes == null) || (indexes.length <= 0)) {
+            return null;
+        } else {
+            Object[] tags = new Object[indexes.length];
+            for (int i = 0; i < indexes.length; i++) {
+
+                tags[i] = getTag(indexes[i]);
+            }
+            return tags;
+        }
+    }
+
+}
