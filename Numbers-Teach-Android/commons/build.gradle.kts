496cf71e151db3de94977fd3f2643e5263942877
FerreiraDaniel <daniel.dos.santos.ferreira@gmail.com>
2021-05-23 23:18:19 +0200

    Migrate to gradle kts (#4)
    * Migration of gradle scripts to kotlin.

---
+++ Numbers-Teach-Android/commons/build.gradle.kts
@@ -0,0 +1,40 @@
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = rootProject.extra["compile_sdk_version"] as Int
+    buildToolsVersion = rootProject.extra["build_tools_version"] as String
+
+    defaultConfig {
+        minSdk = rootProject.extra["minimum_sdk_version"] as Int
+        targetSdk = rootProject.extra["target_sdk_version"] as Int
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompat_version"]}")
+    implementation("androidx.preference:preference-ktx:${rootProject.extra["preference_version"]}")
+    implementation("com.google.android.material:material:${rootProject.extra["material_design_version"]}")
+    implementation("androidx.core:core-ktx:${rootProject.extra["core_version"]}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+}
+
+repositories {
+    mavenCentral()
+}
---
+++ Numbers-Teach-Android/exercise_icons/build.gradle.kts
@@ -0,0 +1,38 @@
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = rootProject.extra["compile_sdk_version"] as Int
+    buildToolsVersion = rootProject.extra["build_tools_version"] as String
+
+    defaultConfig {
+        minSdk = rootProject.extra["minimum_sdk_version"] as Int
+        targetSdk = rootProject.extra["target_sdk_version"] as Int
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompat_version"]}")
+    implementation("androidx.core:core-ktx:${rootProject.extra["core_version"]}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+}
+
+repositories {
+    mavenCentral()
+}
